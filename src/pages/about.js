df9a227e797442d235c652a2ca21671c7c2f1379
JasonNelemans <jasonnelemans@hotmail.com>
2020-06-28 23:04:31 +0200

    created about, blog and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>A driven and passionate developer aiming for greater heights, with a new found love for programming.</p>
+      <p>I have much to learn still, but I am excited to learn all of it.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact details</h1>
+      <p>Jason Nelemans <br/>
+        jasonnelemans@hotmail.com <br/>
+        Amsterdam, the Netherlands. 
+      </p>
+    </div>
+  )
+}
+
+export default ContactPage
