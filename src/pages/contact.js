6258dfa3e745701ae3f7de105abfd6a4b6d8690b
JasonNelemans <jasonnelemans@hotmail.com>
2020-06-29 13:32:08 +0200

    created simple header and footer

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
+
+import Footer from "../components/footer"
+import Header from "../components/header"
 
 const ContactPage = () => {
   return (
     <div>
+      <Header />
       <h1>Contact details</h1>
       <p>Jason Nelemans <br/>
         jasonnelemans@hotmail.com <br/>
         Amsterdam, the Netherlands. <br/>
-        <a href="https://www.linkedin.com/in/jasonnelemans/" target="_blank"> Find me on LinkedIn. </a>
+        <a href="https://www.linkedin.com/in/jasonnelemans/" target="_blank" rel="noreferrer"> Find me on LinkedIn. </a>
       </p>
+      <Footer />
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
+import Footer from "../components/footer"
+import Header from "../components/header"
 
 const IndexPage = () => {
   return (
     <div>
+      <Header />
       <h1>Hello.</h1>
       <h2>I'm Jason, a full-stack developer living in beautiful Amsterdam.</h2>
       <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+      <Footer />
     </div>
   )
 } 
 
 export default IndexPage;
-
-//Time stamp 35:56, on to lesson 4. 
