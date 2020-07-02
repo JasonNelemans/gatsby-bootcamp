411d5171408a830c20ed88707e7d79512e16c3fa
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-02 19:38:28 +0200

    mystery guest contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,20 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
 
 import Layout from "../components/layout"
 
 const ContactPage = () => {
-  return (
-    <div>
-      <Layout>
-        <h1>Contact details</h1>
-        <p>Jason Nelemans <br/>
-          jasonnelemans@hotmail.com <br/>
-          Amsterdam, the Netherlands. <br/>
-          <a href="https://www.linkedin.com/in/jasonnelemans/" target="_blank" rel="noreferrer"> Find me on LinkedIn. </a>
-        </p>
-      </Layout>
-    </div>
+  const [guest, setGuest] = useState("normal")
+
+  const button = (
+    <button
+      onClick={() =>
+        guest === "normal" ? setGuest("mystery") : setGuest("normal")
+      }
+    >
+      Switch
+    </button>
   )
+
+  if (guest === "normal") {
+    return (
+      <div>
+        <Layout>
+          <h1>Contact details</h1>
+          <p>
+            Jason Nelemans <br />
+            jasonnelemans@hotmail.com <br />
+            Amsterdam, the Netherlands. <br />
+            <a
+              href="https://www.linkedin.com/in/jasonnelemans/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Find me on LinkedIn.{" "}
+            </a>
+          </p>
+          {button}
+        </Layout>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <Layout>
+          <h2>Mystery guest</h2>
+          {button}
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default ContactPage
