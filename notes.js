4b4968bc93390d453d1fee281add3c0ba44fb961
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-03 23:23:16 +0200

    add comment

--- notes.js
+++ notes.js
@@ -3,6 +3,10 @@ Check Great Gatsby course at 1:50:00 for how to install Graphql playground
 
 2:22:04: lesson #12. Rewatch this to better understand what slugs are and its uses.
 
+This comment is here to remind to continue working. Sometimes, you feel like you lose motivation. 
+Sometimes, you feel like you lose grip on the internal and let the external shake you. Remember that 
+all the greats had times of doubt. But they were great, because they did not let it last. 
+
 Continue at: 2:52:11, lesson #14. 
 */
 
