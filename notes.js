18f8feeef562bf392cec49aa8f9f498369ef0f74
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-03 23:25:45 +0200

    add second comment, async await

--- notes.js
+++ notes.js
@@ -7,6 +7,12 @@ This comment is here to remind to continue working. Sometimes, you feel like you
 Sometimes, you feel like you lose grip on the internal and let the external shake you. Remember that 
 all the greats had times of doubt. But they were great, because they did not let it last. 
 
+This is a promise:
+Let me become aware of the things that distract me and why I allow them to let it distract me. 
+This is a promise, that I will put in all of my being into my passion and ventures. The pay off is slow, 
+but it's there, waiting for me on the horizon. I will get there. I will persevere. And once it's there, 
+it's going to come flooding. 
+
 Continue at: 2:52:11, lesson #14. 
 */
 
