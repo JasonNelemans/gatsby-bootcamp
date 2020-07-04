744de72e677edaa24826e593c7d4900b1cec4599
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-04 23:46:07 +0200

    finished rendering dynamic content for blog post

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -16,8 +16,11 @@ export const query = graphql`
 `
 
 const Blog = (props) => {
+  console.log('PROPS: ', props)
   return <Layout>
       <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div>
   </Layout>
 }
 
